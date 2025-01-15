097bddb61ddf7a1d085c557218383d3f32e87d00
zsakvo <gdwqsp@gmail.com>
2025-01-15 17:48:06 +0800

    删除 HomeScreen 组件，优化路由配置以使用 ShelfScreen 作为默认显示页面

--- lib/app/ui/router.dart
+++ lib/app/ui/router.dart
@@ -2,39 +2,11 @@ import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:reader/app/ui/components/bottom_nav.dart';
 import 'package:reader/discover/ui/discover_screen.dart';
-import 'package:reader/home/ui/home_screen.dart';
+
 import 'package:reader/preference/ui/preference_screen.dart';
 import 'package:reader/shelf/ui/shelf_screen.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-final _router = GoRouter(
-  navigatorKey: navigatorKey,
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    ),
-    //  GoRoute(
-    //   path: '/',
-    //   builder: (context, state) => const ShelfScreen(),
-    // ),
-    // GoRoute(
-    //   path: '/reader/:name/:aid/:cIndex',
-    //   builder: (context, state) {
-    //     final name = state.pathParameters["name"] as String;
-    //     final aid = state.pathParameters["aid"] as String;
-    //     final cIndex = int.parse(state.pathParameters["cIndex"] as String);
-    //     return ReaderScreen(name: name, aid: aid, cIndex: cIndex);
-    //   },
-    // ),
-    GoRoute(
-        path: '/preference',
-        builder: (context, state) {
-          return const PreferenceScreen();
-        }),
-  ],
-);
 
 class CustomNavigationHelper {
   static final CustomNavigationHelper _instance = CustomNavigationHelper._internal();
