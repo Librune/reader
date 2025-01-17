a837f0f90e767d43b929ddc9d875762735c2d83b
zsakvo <gdwqsp@gmail.com>
2025-01-17 15:50:57 +0800

    新增书源列表页面，创建 BookSourceItem 组件，优化列表布局和样式

--- lib/book_source/ui/book_source_list_screen.dart
+++ lib/book_source/ui/book_source_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reader/app/ui/components/app_top_bar.dart';
 import 'package:reader/app/ui/components/svg_btn.dart';
+import 'package:reader/book_source/ui/components/book_source_item.dart';
 
 class BookSourceListScreen extends HookConsumerWidget {
   const BookSourceListScreen({super.key});
@@ -12,6 +13,19 @@ class BookSourceListScreen extends HookConsumerWidget {
           title: "书源列表",
           actions: [SvgBtn(svgName: "ic_btn_plus")],
         ),
-        body: Container());
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          child: ListView.separated(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return BookSourceItem();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: 12,
+              );
+            },
+          ),
+        ));
   }
 }
