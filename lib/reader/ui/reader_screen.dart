55759cbdff7b1fb57a7d95c65ec2eab1b415a4ce
zsakvo <gdwqsp@gmail.com>
2025-02-08 17:05:15 +0800

    优化代码结构，提高可读性和维护性

--- lib/reader/ui/reader_screen.dart
+++ lib/reader/ui/reader_screen.dart
@@ -20,7 +20,6 @@ import 'package:reader/reader/ui/components/sheets/font.dart';
 import 'package:reader/reader/ui/components/sheets/theme.dart';
 import 'package:reader/reader/ui/components/top_bar.dart';
 import 'package:reader/reader/usecase/menu_sheet_usecase.dart';
-import 'package:reader/reader/usecase/progress_usecase.dart';
 import 'package:reader/reader/usecase/provider_usecase.dart';
 
 import 'components/render.dart';
@@ -40,9 +39,7 @@ class _ReaderScreenState extends ConsumerState<ReaderScreen> {
       ProviderUsecase().init(book: widget.book, context: context);
       return null;
     }, []);
-    // final provider = readerProvider(book, context: context);
     final reader = ref.watch(ProviderUsecase().reader);
-    // final colorScheme = ColorScheme.fromSeed(seedColor: Color(0xFFFFDE3F));
     final themeId = ref.watch(ProviderUsecase().config.select((value) => value.theme));
     final themeModel = ReaderThemeModel.fromJson(
         jsonDecode(File(join(PathService().readerThemesPath, themeId, 'index.json')).readAsStringSync()));
@@ -53,7 +50,6 @@ class _ReaderScreenState extends ConsumerState<ReaderScreen> {
         canPop: false,
         onPopInvokedWithResult: (didPop, result) {
           if (ref.read(ProviderUsecase().menu.select((value) => value.sub))) {
-            // MenuSheetUsecase().close();
             MenuSheetUsecase().toggleMenu(ref);
           } else if (ref.read(ProviderUsecase().menu.select((value) => value.bottom))) {
             ref.read(ProviderUsecase().menu.notifier).closeBottom();
@@ -63,130 +59,140 @@ class _ReaderScreenState extends ConsumerState<ReaderScreen> {
           }
         },
         child: Theme(
-          data: ThemeData(colorScheme: colorScheme),
-          child: Material(
+            data: ThemeData(colorScheme: colorScheme),
+            child: Material(
               color: colorScheme.surfaceContainerHigh,
-              child: Stack(
-                children: [
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        Log.f('constraints: $constraints');
-                        return reader.when(
-                          data: (value) {
-                            return Container(
-                                decoration: BoxDecoration(
-                                  image: backgroundPngFile.existsSync()
-                                      ? DecorationImage(
-                                          image: FileImage(backgroundPngFile),
-                                          fit: BoxFit.cover,
-                                        )
-                                      : null,
-                                  color: colorScheme.surfaceContainer.withAlpha(180),
-                                ),
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.height,
-                                child: PageSlider(
-                                  itemBuilder: (context, index) {
-                                    return ReaderPage(pagePainter: value[index], context: context);
-                                  },
-                                  controller: ref.read(ProviderUsecase().reader.notifier).pageSliderController,
-                                  itemCount: value.length,
-                                  onPageChanged: (index) {
-                                    ref.read(ProviderUsecase().reader.notifier).onPageChange(index);
-                                  },
-                                  toggleMenu: () {
-                                    MenuSheetUsecase().toggleMenu(ref);
-                                  },
-                                ));
-                          },
-                          error: (error, stackTrace) {
-                            return Center(
-                              child: Text(
-                                error.toString(),
-                                style: TextStyle(color: colorScheme.onSurface.withAlpha(150), fontSize: 16),
-                              ),
-                            );
-                          },
-                          loading: () {
-                            return Center(
-                              child: Text(
-                                "正在加载……",
-                                style: TextStyle(color: colorScheme.onSurface.withAlpha(150), fontSize: 16),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
+              child: Container(
+                  decoration: BoxDecoration(
+                    image: backgroundPngFile.existsSync()
+                        ? DecorationImage(
+                            image: FileImage(backgroundPngFile),
+                            fit: BoxFit.cover,
+                          )
+                        : null,
                   ),
-                  // Consumer(
-                  //   builder: (context, ref, child) {
-                  //     final subVisible = ref.watch(ProviderUsecase().menu.select((value) => value.sub));
-                  //     return IgnorePointer(
-                  //       ignoring: !subVisible,
-                  //       child: Scaffold(
-                  //         backgroundColor: Colors.transparent,
-                  //         key: MenuSheetUsecase().sheetCtx,
-                  //         body: GestureDetector(
-                  //           onTap: () {
-                  //             MenuSheetUsecase().close();
-                  //           },
-                  //         ),
-                  //       ),
-                  //     );
-                  //   },
-                  // ),
-                  TopBar(book: widget.book),
-                  PersistentBottomSheet(
-                      type: ReaderBottomSheet.catalog,
-                      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 64),
-                      key: MenuSheetUsecase().catalogSheetKey,
-                      maxHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top - 48,
-                      onHide: (triggeredByDrag) {
-                        MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().catalogSheetKey,
-                            ref: ref, triggeredByDrag: triggeredByDrag);
-                      },
-                      child: CatalogSheetContent()),
-                  PersistentBottomSheet(
-                      type: ReaderBottomSheet.font,
-                      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 50, left: 4, right: 4),
-                      key: MenuSheetUsecase().fontSheetKey,
-                      maxHeight: 360,
-                      onHide: (triggeredByDrag) {
-                        MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().fontSheetKey,
-                            ref: ref, triggeredByDrag: triggeredByDrag);
-                      },
-                      child: FontSheetContent()),
-                  PersistentBottomSheet(
-                      type: ReaderBottomSheet.theme,
-                      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 72),
-                      key: MenuSheetUsecase().themeSheetKey,
-                      maxHeight: 600,
-                      onHide: (triggeredByDrag) {
-                        MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().themeSheetKey,
-                            ref: ref, triggeredByDrag: triggeredByDrag);
-                      },
-                      child: ThemeSheetContent()),
-                  PersistentBottomSheet(
-                      type: ReaderBottomSheet.config,
-                      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 72),
-                      key: MenuSheetUsecase().configSheetKey,
-                      maxHeight: MediaQuery.of(context).size.height / 2,
-                      onHide: (triggeredByDrag) {
-                        MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().configSheetKey,
-                            ref: ref, triggeredByDrag: triggeredByDrag);
-                      },
-                      child: ConfigSheetContent()),
-                  BottomBar(),
-                ],
-              )),
-        ));
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        child: LayoutBuilder(
+                          builder: (context, constraints) {
+                            Log.f('constraints: $constraints');
+                            return reader.when(
+                              data: (value) {
+                                return Container(
+                                    decoration: BoxDecoration(
+                                      image: backgroundPngFile.existsSync()
+                                          ? DecorationImage(
+                                              image: FileImage(backgroundPngFile),
+                                              fit: BoxFit.cover,
+                                            )
+                                          : null,
+                                      color: colorScheme.surfaceContainer.withAlpha(180),
+                                    ),
+                                    width: MediaQuery.of(context).size.width,
+                                    height: MediaQuery.of(context).size.height,
+                                    child: PageSlider(
+                                      itemBuilder: (context, index) {
+                                        return ReaderPage(pagePainter: value[index], context: context);
+                                      },
+                                      controller: ref.read(ProviderUsecase().reader.notifier).pageSliderController,
+                                      itemCount: value.length,
+                                      onPageChanged: (index) {
+                                        ref.read(ProviderUsecase().reader.notifier).onPageChange(index);
+                                      },
+                                      toggleMenu: () {
+                                        MenuSheetUsecase().toggleMenu(ref);
+                                      },
+                                    ));
+                              },
+                              error: (error, stackTrace) {
+                                return Center(
+                                  child: Text(
+                                    error.toString(),
+                                    style: TextStyle(color: colorScheme.onSurface.withAlpha(150), fontSize: 16),
+                                  ),
+                                );
+                              },
+                              loading: () {
+                                return Center(
+                                  child: Text(
+                                    "正在加载……",
+                                    style: TextStyle(color: colorScheme.onSurface.withAlpha(150), fontSize: 16),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      // Consumer(
+                      //   builder: (context, ref, child) {
+                      //     final subVisible = ref.watch(ProviderUsecase().menu.select((value) => value.sub));
+                      //     return IgnorePointer(
+                      //       ignoring: !subVisible,
+                      //       child: Scaffold(
+                      //         backgroundColor: Colors.transparent,
+                      //         key: MenuSheetUsecase().sheetCtx,
+                      //         body: GestureDetector(
+                      //           onTap: () {
+                      //             MenuSheetUsecase().close();
+                      //           },
+                      //         ),
+                      //       ),
+                      //     );
+                      //   },
+                      // ),
+                      TopBar(book: widget.book),
+                      PersistentBottomSheet(
+                          type: ReaderBottomSheet.catalog,
+                          padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 64),
+                          key: MenuSheetUsecase().catalogSheetKey,
+                          maxHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top - 48,
+                          onHide: (triggeredByDrag) {
+                            MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().catalogSheetKey,
+                                ref: ref, triggeredByDrag: triggeredByDrag);
+                          },
+                          child: CatalogSheetContent()),
+                      PersistentBottomSheet(
+                          type: ReaderBottomSheet.font,
+                          padding:
+                              EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 50, left: 4, right: 4),
+                          key: MenuSheetUsecase().fontSheetKey,
+                          maxHeight: 360,
+                          onHide: (triggeredByDrag) {
+                            MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().fontSheetKey,
+                                ref: ref, triggeredByDrag: triggeredByDrag);
+                          },
+                          child: FontSheetContent()),
+                      PersistentBottomSheet(
+                          type: ReaderBottomSheet.theme,
+                          padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 72),
+                          key: MenuSheetUsecase().themeSheetKey,
+                          maxHeight: 600,
+                          onHide: (triggeredByDrag) {
+                            MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().themeSheetKey,
+                                ref: ref, triggeredByDrag: triggeredByDrag);
+                          },
+                          child: ThemeSheetContent()),
+                      PersistentBottomSheet(
+                          type: ReaderBottomSheet.config,
+                          padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 72),
+                          key: MenuSheetUsecase().configSheetKey,
+                          maxHeight: MediaQuery.of(context).size.height / 2,
+                          onHide: (triggeredByDrag) {
+                            MenuSheetUsecase().onSheetDragHide(MenuSheetUsecase().configSheetKey,
+                                ref: ref, triggeredByDrag: triggeredByDrag);
+                          },
+                          child: ConfigSheetContent()),
+                      BottomBar(),
+                    ],
+                  )),
+            )));
   }
 }
