0c19ccfffb2f98bf4054ac15b54ecb5db03beebb
zsakvo <gdwqsp@gmail.com>
2025-02-19 18:00:20 +0800

    重构 Jsoup 适配器，简化 Document 和 Element 包装，优化全局函数注册和方法定义

--- rust/src/ecma/libs/scraper/jsoup_adapter.rs
+++ rust/src/ecma/libs/scraper/jsoup_adapter.rs
@@ -1,263 +1,99 @@
 use super::scraper_adapter::{JsDocument, JsElement};
 use boa_engine::{
-    object::{Object, ObjectInitializer},
-    property::Attribute,
-    value::ValueContext,
-    Context, JsFunction, JsNativeError, JsObject, JsResult, JsValue,
+    js_error, js_string, object::ObjectInitializer, Context, JsError, JsResult, JsValue,
+    NativeFunction,
 };
-use boa_gc::{Finalize, Trace};
 
-// Document包装器
-#[derive(Debug, Trace, Finalize)]
-pub struct JsDocumentWrapper {
-    inner: JsDocument,
-}
-
-impl JsDocumentWrapper {
-    fn new(html: &str) -> Self {
-        Self {
-            inner: JsDocument::parse(html),
-        }
-    }
-}
-
-// Element包装器
-#[derive(Debug, Trace, Finalize)]
-pub struct JsElementWrapper {
-    inner: JsElement,
-}
-
-impl JsElementWrapper {
-    fn new(element: JsElement) -> Self {
-        Self { inner: element }
-    }
-}
+// Document和Element的简单包装
+struct JsDocumentWrapper(JsDocument);
+struct JsElementWrapper(JsElement);
 
 pub fn init_jsoup(context: &mut Context) -> JsResult<()> {
-    // 创建Document原型对象
-    let document_proto = context.object_prototype();
-
-    // Document构造函数
-    let document_constructor = JsFunction::new(
-        context,
-        |_, args, context| {
-            let html = args
-                .get(0)
-                .ok_or_else(|| {
-                    JsNativeError::typ().with_message("Document constructor requires HTML string")
-                })?
-                .to_string(context)?;
-
-            let wrapper = JsDocumentWrapper::new(&html);
-            let obj = Object::new(context);
-            obj.define_property_or_throw(
-                "inner",
-                wrapper,
-                Attribute::WRITABLE | Attribute::ENUMERABLE,
-                context,
-            )?;
-
-            Ok(obj.into())
-        },
-        Some("Document"),
-        1,
-    )?;
-
-    // Document原型方法
-    let document_prototype = document_constructor.prototype(context)?;
-
-    // select方法
-    document_prototype.define_native_method(
-        "select",
-        1,
-        |this, args, context| {
-            let wrapper = this
-                .as_object()
-                .and_then(|obj| obj.get_property("inner").ok())
-                .and_then(|val| val.as_object())
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Document instance"))?;
-
-            let selector = args
-                .get(0)
-                .ok_or_else(|| JsNativeError::typ().with_message("Selector string required"))?
-                .to_string(context)?;
-
-            let doc_wrapper: &JsDocumentWrapper = wrapper
-                .downcast_ref()
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Document instance"))?;
-
-            match doc_wrapper.inner.select(&selector) {
-                Ok(elements) => {
-                    let array = context.construct_array()?;
-                    for (i, elem) in elements.into_iter().enumerate() {
-                        let element_wrapper = JsElementWrapper::new(elem);
-                        let element_obj = create_element_object(element_wrapper, context)?;
-                        array.set(i as u32, element_obj.into(), false, context)?;
-                    }
-                    Ok(array.into())
-                }
-                Err(e) => Err(JsNativeError::typ().with_message(&e).into()),
-            }
-        },
-        context,
-    )?;
-
-    // 注册Document构造函数为全局对象
-    context.register_global_property(
-        "Document",
-        document_constructor.into(),
-        Attribute::WRITABLE | Attribute::ENUMERABLE | Attribute::CONFIGURABLE,
-    )?;
-
-    // 创建全局parse函数
-    let parse_fn = JsFunction::new(
-        context,
-        |_, args, context| {
-            let html = args
-                .get(0)
-                .ok_or_else(|| JsNativeError::typ().with_message("parseHtml requires HTML string"))?
-                .to_string(context)?;
-
-            let wrapper = JsDocumentWrapper::new(&html);
-            let obj = Object::new(context);
-            obj.define_property_or_throw(
-                "inner",
-                wrapper,
-                Attribute::WRITABLE | Attribute::ENUMERABLE,
-                context,
-            )?;
-
-            Ok(obj.into())
-        },
-        Some("parseHtml"),
-        1,
-    )?;
-
     // 注册全局parse函数
-    context.register_global_property(
-        "parseHtml",
-        parse_fn.into(),
-        Attribute::WRITABLE | Attribute::ENUMERABLE | Attribute::CONFIGURABLE,
+    context.register_global_builtin_callable(
+        js_string!("parseHtml"),
+        1,
+        NativeFunction::from_fn_ptr(|_, args, context| {
+            let html = args
+                .get(0)
+                .and_then(|v| v.as_string())
+                .ok_or_else(|| js_error!("parseHtml requires HTML string"))?;
+
+            let doc = JsDocument::parse(html.to_std_string_escaped().clone());
+            let obj = context.object_prototype();
+
+            // 添加select方法
+            obj.set_method("select", 1, |_, args, context| {
+                let selector = args
+                    .get(0)
+                    .and_then(|v| v.as_string())
+                    .ok_or_else(|| js_error!("Selector string required", context))?;
+
+                match doc.select(&selector) {
+                    Ok(elements) => {
+                        let array = context.array();
+                        for (i, elem) in elements.into_iter().enumerate() {
+                            let elem_obj = create_element_object(elem, context)?;
+                            array.set_index(i as u32, elem_obj)?;
+                        }
+                        Ok(array.into())
+                    }
+                    Err(e) => Err(js_error!("Failed to select elements: {}", e)),
+                }
+            })?;
+
+            Ok(obj.into())
+        }),
     )?;
 
     Ok(())
 }
 
-// 创建Element对象的辅助函数
-fn create_element_object(wrapper: JsElementWrapper, context: &mut Context) -> JsResult<JsObject> {
-    let obj = Object::new(context);
-
-    // 存储wrapper
-    obj.define_property_or_throw(
-        "inner",
-        wrapper,
-        Attribute::WRITABLE | Attribute::ENUMERABLE,
-        context,
-    )?;
+fn create_element_object(element: JsElement, context: &mut Context) -> JsResult<JsValue> {
+    let obj = context.intrinsics().constructors().object().prototype();
 
     // attr方法
-    obj.define_native_method(
-        "attr",
-        1,
-        |this, args, context| {
-            let wrapper = this
-                .as_object()
-                .and_then(|obj| obj.get_property("inner").ok())
-                .and_then(|val| val.as_object())
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
+    obj.set_method("attr", 1, move |_, args, context| {
+        let name = args
+            .get(0)
+            .and_then(|v| v.as_string())
+            .ok_or_else(|| js_error!("Attribute name required"))?;
 
-            let name = args
-                .get(0)
-                .ok_or_else(|| JsNativeError::typ().with_message("Attribute name required"))?
-                .to_string(context)?;
-
-            let element_wrapper: &JsElementWrapper = wrapper
-                .downcast_ref()
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
-
-            Ok(match element_wrapper.inner.attr(&name) {
-                Some(value) => JsValue::from(value),
-                None => JsValue::null(),
-            })
-        },
-        context,
-    )?;
+        Ok(match element.attr(&name) {
+            Some(value) => JsValue::String(value.into()),
+            None => JsValue::Null,
+        })
+    })?;
 
     // text方法
-    obj.define_native_method(
-        "text",
-        0,
-        |this, _args, context| {
-            let wrapper = this
-                .as_object()
-                .and_then(|obj| obj.get_property("inner").ok())
-                .and_then(|val| val.as_object())
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
-
-            let element_wrapper: &JsElementWrapper = wrapper
-                .downcast_ref()
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
-
-            Ok(JsValue::from(element_wrapper.inner.text()))
-        },
-        context,
-    )?;
+    obj.set_method("text", 0, move |_, _, _| {
+        Ok(JsValue::String(element.text().into()))
+    })?;
 
     // html方法
-    obj.define_native_method(
-        "html",
-        0,
-        |this, _args, context| {
-            let wrapper = this
-                .as_object()
-                .and_then(|obj| obj.get_property("inner").ok())
-                .and_then(|val| val.as_object())
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
-
-            let element_wrapper: &JsElementWrapper = wrapper
-                .downcast_ref()
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
-
-            Ok(JsValue::from(element_wrapper.inner.html()))
-        },
-        context,
-    )?;
+    obj.set_method("html", 0, move |_, _, _| {
+        Ok(JsValue::String(element.html().into()))
+    })?;
 
     // select方法
-    obj.define_native_method(
-        "select",
-        1,
-        |this, args, context| {
-            let wrapper = this
-                .as_object()
-                .and_then(|obj| obj.get_property("inner").ok())
-                .and_then(|val| val.as_object())
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
+    obj.set_method("select", 1, move |_, args, context| {
+        let selector = args
+            .get(0)
+            .and_then(|v| v.as_string())
+            .ok_or_else(|| js_error!("Selector string required"))?;
 
-            let selector = args
-                .get(0)
-                .ok_or_else(|| JsNativeError::typ().with_message("Selector string required"))?
-                .to_string(context)?;
-
-            let element_wrapper: &JsElementWrapper = wrapper
-                .downcast_ref()
-                .ok_or_else(|| JsNativeError::typ().with_message("Invalid Element instance"))?;
-
-            match element_wrapper.inner.select(&selector) {
-                Ok(elements) => {
-                    let array = context.construct_array()?;
-                    for (i, elem) in elements.into_iter().enumerate() {
-                        let element_wrapper = JsElementWrapper::new(elem);
-                        let element_obj = create_element_object(element_wrapper, context)?;
-                        array.set(i as u32, element_obj.into(), false, context)?;
-                    }
-                    Ok(array.into())
+        match element.select(&selector) {
+            Ok(elements) => {
+                let array = context.array();
+                for (i, elem) in elements.into_iter().enumerate() {
+                    let elem_obj = create_element_object(elem, context)?;
+                    array.set_index(i as u32, elem_obj)?;
                 }
-                Err(e) => Err(JsNativeError::typ().with_message(&e).into()),
+                Ok(array.into())
             }
-        },
-        context,
-    )?;
+            Err(e) => Err(js_error!("Failed to select elements: {}", e)),
+        }
+    })?;
 
-    Ok(obj)
+    Ok(obj.into())
 }
