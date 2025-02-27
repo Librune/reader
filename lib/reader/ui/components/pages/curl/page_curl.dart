00f538331b94d6684dde9c86a6f5b603149a1c14
zsakvo <gdwqsp@gmail.com>
2025-02-27 22:37:36 +0800

    新增 PageCurl 组件及其着色器，支持页面翻转效果

---
+++ lib/reader/ui/components/pages/curl/page_curl.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PageCurl extends StatefulHookConsumerWidget {
+  const PageCurl({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _PageCurlState();
+}
+
+class _PageCurlState extends ConsumerState<PageCurl> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
