fff2aadf285046a2cb008d879b0a3b5a1b6cc020
zsakvo <gdwqsp@gmail.com>
2025-03-03 00:14:56 +0800

    重构侧边栏组件，删除未使用的图标，新增书籍和发现图标，优化图标加载方式

--- apps/macos/lib/home/components/sidebar.dart
+++ apps/macos/lib/home/components/sidebar.dart
@@ -3,14 +3,17 @@ import 'package:flutter_svg/svg.dart';
 import 'package:macos_ui/macos_ui.dart';
 
 class MacosSidebarItem extends SidebarItem {
-  MacosSidebarItem()
+  MacosSidebarItem({required this.labelText, required this.iconName})
     : super(
         selectedColor: MacosColors.tickBackgroundColor,
         leading: SvgPicture.asset(
-          "assets/svg/ic_bottom_books.svg",
+          "assets/svg/$iconName.svg",
           width: 18,
-          colorFilter: ColorFilter.mode(MacosColors.placeholderTextColor, BlendMode.srcIn),
+          colorFilter: ColorFilter.mode(MacosColors.systemGrayColor, BlendMode.srcIn),
         ),
-        label: Text('书架', style: TextStyle(fontSize: 13)),
+        label: Text(labelText, style: TextStyle(fontSize: 12)),
       );
+
+  final String labelText;
+  final String iconName;
 }
