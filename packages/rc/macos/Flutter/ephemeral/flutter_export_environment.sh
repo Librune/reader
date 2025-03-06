ad1260294c0aa5f102b40ebf165c12e1ba1d704a
zsakvo <gdwqsp@gmail.com>
2025-03-07 00:03:33 +0800

    更新 rc 包版本至 0.0.1，修正应用路径为项目目录

--- packages/rc/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ packages/rc/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/zsakvo/fvm/versions/stable"
-export "FLUTTER_APPLICATION_PATH=/Users/zsakvo/Temp/plugin_test/rc"
+export "FLUTTER_APPLICATION_PATH=/Users/zsakvo/Project/reader/packages/rc"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
