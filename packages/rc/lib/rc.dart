04cbdf15542f692f0c0fb16eaeac606977afed0b
zsakvo <gdwqsp@gmail.com>
2025-03-07 14:23:44 +0800

    更新 .gitignore 文件，添加构建相关目录；修改 pubspec.yaml 以禁用发布；移除生成的插件注册文件；清理不再需要的示例和配置文件

--- packages/rc/lib/rc.dart
+++ packages/rc/lib/rc.dart
@@ -1,14 +1,5 @@
-// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
-
-// /// A Calculator.
-// class Calculator {
-//   /// Returns [value] plus 1.
-//   int addOne(int value) => value + 1;
-// }
-//
-
 library;
 
-export 'src/rust/api/ecma.dart';
 export 'src/rust/api/envs.dart';
+export 'src/rust/api/ecma.dart';
 export 'src/rust/frb_generated.dart' show Rc;
